1203e1c49b46c4c8899e011827dd4ff218011dfa
WOIEhtgi3eh45tg4h598yh459tyh4985yh9845y <136754444+WOIEhtgi3eh45tg4h598yh459tyh4985yh9845y@users.noreply.github.com>
2023-07-06 20:13:28 +0200

    Update 34tyhj4895hy9845hy89h4589yh4589yh89345hy8945n8yu894buv589y84596u.lua

--- Fishing Frenzy Misc/34tyhj4895hy9845hy89h4589yh4589yh89345hy8945n8yu894buv589y84596u.lua
+++ Fishing Frenzy Misc/34tyhj4895hy9845hy89h4589yh4589yh89345hy8945n8yu894buv589y84596u.lua
@@ -1,3 +1,3 @@
-setclipboard("https://discord.gg/wBMat4nDc7")
-toclipboard("https://discord.gg/wBMat4nDc7")
-game.Players.LocalPlayer:Kick("You're using an outdated version! Join https://discord.gg/wBMat4nDc7")
+setclipboard("https://discord.gg/vr2BUGfr9k")
+toclipboard("https://discord.gg/vr2BUGfr9k")
+game.Players.LocalPlayer:Kick("You're using an outdated version! Join https://discord.gg/vr2BUGfr9k")
