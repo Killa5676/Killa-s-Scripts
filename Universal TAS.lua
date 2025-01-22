08a694ed6adb60245e49b85e6a0ed242a8481c3e
Killa <119937376+Killa5676@users.noreply.github.com>
2025-01-22 20:12:42 +0000

    Update Universal TAS.lua

--- Universal TAS.lua
+++ Universal TAS.lua
@@ -1090,8 +1090,8 @@ do
 
 					local CurrentState = Util:GetHumanoid():GetState().Value
 
-					--Camera:SetCFrame(currentCameraCFrame)
-					--Camera:SetZoom(zoom)
+					Camera:SetCFrame(currentCameraCFrame)
+					Camera:SetZoom(zoom)
 
 					Util:GetHumanoid():ChangeState(humanoidState)
 
@@ -1112,20 +1112,18 @@ do
 					pcall(function()
 						Animate:SetAnimationSpeed(animationSpeed)
 					end)
---[[
+
 					if shiftLockEnabled then
 						Input:SetShiftLockEnabled(true)
 					else
 						Input:SetShiftLockEnabled(false)
 					end
-					--]]
---[[
+
 					if not shiftLockEnabled and zoom > 0.52 then
 						mousemoveabs(mouseLocation.X, mouseLocation.Y)
 					else
 						mousemoveabs((Input.Resolution.X / 2) + Input.CursorOffset.X - GuiInset.X, (Input.Resolution.Y / 2) + Input.CursorOffset.Y - GuiInset.Y - 36)
 					end
-					--]]
 
 					Util:GetCharacter().HumanoidRootPart.CFrame = rootPartCFrame
 
@@ -1474,7 +1472,7 @@ do
 
 		CurrentCamera.Changed:Connect(function()
 			if Replay.Reading then
-				--CurrentCamera.CFrame = Camera.CameraCFrame
+				CurrentCamera.CFrame = Camera.CameraCFrame
 			end
 		end)
 
